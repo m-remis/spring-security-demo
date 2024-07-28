4f48cd3293719000c0c06bb06776866a8fe34241
Michal Remis <michal.remis@globallogic.com>
2024-07-28 17:48:36 +0200

    readme

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# spring-security
+![Java](https://img.shields.io/badge/Java-21-orange)
+![Maven](https://img.shields.io/badge/Maven%20-8A2BE2)
+![Spring](https://img.shields.io/badge/Spring_Boot%20-green)
+![H2](https://img.shields.io/badge/H2%20-blue)
+
+
+### Basic spring security demo
+
+#### Simple example application
+    - simple in memory users
+    - just basic authentication
+    - role based authorization
--- src/main/java/com/michal/examples/spring/security/controller/ControllerExceptionHandler.java
+++ src/main/java/com/michal/examples/spring/security/controller/ControllerExceptionHandler.java
@@ -1,18 +1,15 @@
 package com.michal.examples.spring.security.controller;
 
 import com.michal.examples.spring.security.exception.CardNotFoundException;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
 public class ControllerExceptionHandler {
 
     @ExceptionHandler({CardNotFoundException.class})
-    @ResponseStatus(HttpStatus.I_AM_A_TEAPOT)
     @ResponseBody
     public ResponseEntity<Void> handleException(CardNotFoundException exception) {
         return ResponseEntity.notFound().build();
